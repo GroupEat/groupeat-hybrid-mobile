ee516f7625e559846547ab617476c2ea0fcee373
etiennedemontalivet <etienne.demontalivet@gmail.com>
2015-01-03 22:32:03 +0100

    footer about total cart price added

--- app/scripts/controllers/cart.js
+++ app/scripts/controllers/cart.js
@@ -1,15 +1,23 @@
 'use strict';
 
-angular.module('groupeat.controllers.cart', ['groupeat.services.cart'])
+angular.module('groupeat.controllers.cart', ['groupeat.services.cart', 'groupeat.services.lodash' ])
 
-.controller('CartCtrl', function($scope, $state, Cart) {
+.controller('CartCtrl', function($scope, $state, Cart, _) {
 
-	$scope.cart = Cart.query();
+	$scope.cart = Cart.query(function(cart) {
+		_.forEach(cart, function(product) {
+			$scope.cartTotalPrice += product.price*product.number;
+		});
+	});
 
 	$scope.data = {
-		showDelete: false
+		showDeleteItem: false
 	};
-  
-  
+
+	$scope.onConfirmCommandTouch = function() {
+		console.log(cartTotalPrice);
+	};
+	
+	
 
 });
