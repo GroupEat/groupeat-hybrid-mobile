195e8cdb68f7fd19a44479ac3da30079e3a2e6c7
Woody Rousseau <woody.rousseau@ensta-paristech.fr>
2015-01-13 12:42:26 +0100

    Fixed jshint for error message resolver

--- app/scripts/services/error-message-resolver.js
+++ app/scripts/services/error-message-resolver.js
@@ -2,93 +2,94 @@
 
 angular.module('groupeat.services.error-message-resolver', ['sprintf'])
 
+/*global vsprintf:true*/
 .factory('ErrorMessageResolver', [
-'$q',
-'$filter',
-function ($q, $filter) {
+  '$q',
+  '$filter',
+  function ($q, $filter) {
 
-  var $translate = $filter('translate');
+    var $translate = $filter('translate');
 
-  var isInteger = function(data) {
-    var n = ~~Number(data);
-    return String(n) === data && n >= 0;
-  }
+    var isInteger = function(data) {
+      var n = ~~Number(data);
+      return String(n) === data && n >= 0;
+    };
 
-  var attributesWithMandatoryValues = {'minlength': isInteger, 'maxlength': isInteger};
+    var attributesWithMandatoryValues = {'minlength': isInteger, 'maxlength': isInteger};
 
-  /**
-  * @ngdoc function
-  * @name ErrorMessageResolver#formatErrorMessage
-  * @methodOf ErrorMessageResolver
-  *
-  * @description
-  * Formats the error message so that values given for angular validation rules are inserted
-  * into the error message
-  *
-  * @param {String} errorType - The type of validation error that has occurred.
-  * @param {Element} el - The input element that is the source of the validation error.
-  * @param {String} errorMessage - The error message with eventual placeholders for inserting validation attribute values
-  * @returns {String} The error message with the validation attribute values inserted
-  */
-  var formatErrorMessage = function (errorType, el, errorMessage) {
-    var parameter,
-    parameters = [];
-    if (el && el.attr) {
-      parameter = el.attr(errorType);
-      if (parameter === undefined) {
-        parameter = el.attr('data-ng-' + errorType) || el.attr('ng-' + errorType);
+    /**
+    * @ngdoc function
+    * @name ErrorMessageResolver#formatErrorMessage
+    * @methodOf ErrorMessageResolver
+    *
+    * @description
+    * Formats the error message so that values given for angular validation rules are inserted
+    * into the error message
+    *
+    * @param {String} errorType - The type of validation error that has occurred.
+    * @param {Element} el - The input element that is the source of the validation error.
+    * @param {String} errorMessage - The error message with eventual placeholders for inserting validation attribute values
+    * @returns {String} The error message with the validation attribute values inserted
+    */
+    var formatErrorMessage = function (errorType, el, errorMessage) {
+      var parameter,
+      parameters = [];
+      if (el && el.attr) {
+        parameter = el.attr(errorType);
+        if (parameter === undefined) {
+          parameter = el.attr('data-ng-' + errorType) || el.attr('ng-' + errorType);
+        }
+
+        if (errorType in attributesWithMandatoryValues)
+        {
+          if (!parameter)
+          {
+            throw new Error($translate('missingMandatoryAttributeValueError'));
+          }
+          else if (!attributesWithMandatoryValues[errorType](parameter))
+          {
+            throw new Error($translate('invalidMandatoryAttributeValueTypeError'));
+          }
+        }
+
+        parameters.push(parameter || '');
+
+        return vsprintf(errorMessage, parameters);
       }
+      return errorMessage;
+    };
 
-      if (errorType in attributesWithMandatoryValues)
+    /**
+    * @ngdoc function
+    * @name ErrorMessageResolver#resolve
+    * @methodOf ErrorMessageResolver
+    *
+    * @description
+    * Resolves a validate error type into a user validation error message
+    *
+    * @param {String} errorType - The type of validation error that has occurred.
+    * @param {Element} el - The input element that is the source of the validation error.
+    * @returns {Promise} A promise that is resolved when the validation message has been produced.
+    */
+    var resolve = function (errorType, el) {
+      var defer = $q.defer();
+      if (!el[0].name)
       {
-        if (!parameter)
-        {
-          throw new Error($translate('missingMandatoryAttributeValueError'));
-        }
-        else if (!attributesWithMandatoryValues[errorType](parameter))
-        {
-          throw new Error($translate('invalidMandatoryAttributeValueTypeError'));
-        }
+        defer.reject(new Error($translate('missingFieldNameError')));
       }
+      else
+      {
+        var fieldName = $translate(el[0].name+'FieldName');
+        var errorMessage = $translate(errorType+'ErrorKey', { fieldName: fieldName });
+        errorMessage = formatErrorMessage(errorType, el, errorMessage);
+        defer.resolve(errorMessage);
+      }
+      return defer.promise;
+    };
 
-      parameters.push(parameter || '');
-
-      return vsprintf(errorMessage, parameters);
-    }
-    return errorMessage;
-  };
-
-  /**
-  * @ngdoc function
-  * @name ErrorMessageResolver#resolve
-  * @methodOf ErrorMessageResolver
-  *
-  * @description
-  * Resolves a validate error type into a user validation error message
-  *
-  * @param {String} errorType - The type of validation error that has occurred.
-  * @param {Element} el - The input element that is the source of the validation error.
-  * @returns {Promise} A promise that is resolved when the validation message has been produced.
-  */
-  var resolve = function (errorType, el) {
-    var defer = $q.defer();
-    if (!el[0].name)
-    {
-      defer.reject(new Error($translate('missingFieldNameError')));
-    }
-    else
-    {
-      var fieldName = $translate(el[0].name+'FieldName');
-      var errorMessage = $translate(errorType+'ErrorKey', { fieldName: fieldName });
-      errorMessage = formatErrorMessage(errorType, el, errorMessage);
-      defer.resolve(errorMessage);
-    }
-    return defer.promise;
-  };
-
-  return {
-    formatErrorMessage: formatErrorMessage,
-    resolve: resolve
-  };
-}
+    return {
+      formatErrorMessage: formatErrorMessage,
+      resolve: resolve
+    };
+  }
 ]);
