62755ab71d682043234a1b1c744f91d815639faa
PrincessZorg <sammy.teillet@gmail.com>
2015-01-13 17:01:04 +0100

    Settings with itemselected

--- app/scripts/controllers/settings.js
+++ app/scripts/controllers/settings.js
@@ -13,12 +13,41 @@ angular.module('groupeat.controllers.settings', [])
 	    { name: 'pushSettings', state:'settings-notifications'}
 		];
 
-	$scope.user = {profile: {
-		firstName:   'Phillipe',
-		lastName:    'Fessier',
-		phoneNumber: '0685958687',
-		adress:      'Palaiseau',
-	}};
+	$scope.user = {
+		profile: {
+			firstName:   'Phillipe',
+			lastName:    'Fessier',
+			phoneNumber: '0685958687',
+			adress:      'Palaiseau',
+		},
+		settings: {
+			pushActivation : true,
+			dontPushAfter : 4,
+			dontPushFor : 6,
+		}
+	};
+
+
+	$scope.settings = {};
+
+	$scope.settings.dontPushAfter = [
+		{name: '21h00', id:0},
+		{name: '22h00', id:1},
+		{name: '23h00', id:2},
+		{name: '00h00', id:3},
+		{name: '1h00', id:4}
+	];
+
+	$scope.settings.dontPushFor = [
+		{name: '1 jour', id:0},
+		{name: '2 jours', id:1},
+		{name: '3 jours', id:2},
+		{name: '4 jours', id:3},
+		{name: '5 jours', id:4},
+		{name: '6 jours', id:5},
+		{name: '1 semaine', id:6}
+	];
+
 
 	/*
 	---------------- Profile Editing ----------------------------------
