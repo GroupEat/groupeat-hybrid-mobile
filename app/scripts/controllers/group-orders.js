8c5eb19ba2efc6f0d5c87fe81c462850a8076001
etiennedemontalivet <etienne.demontalivet@gmail.com>
2015-01-24 19:42:53 +0100

    add ngGeolocation to know where user is. Just added in groupOrders ctrl for the moment.console log show current latitude and longitude

--- app/scripts/controllers/group-orders.js
+++ app/scripts/controllers/group-orders.js
@@ -1,10 +1,19 @@
 'use strict';
 
-angular.module('groupeat.controllers.group-orders', ['groupeat.services.group-order', 'groupeat.services.order', 'timer'])
+angular.module('groupeat.controllers.group-orders', ['groupeat.services.group-order', 'groupeat.services.order', 'timer', 'ngGeolocation'])
 
-.controller('GroupOrdersCtrl', function($scope, $state, GroupOrder, Order) {
+.controller('GroupOrdersCtrl', function($scope, $state, GroupOrder, Order, $geolocation) {
 
   $scope.groupOrders = GroupOrder.query();
+
+  $geolocation.getCurrentPosition().then(function(currentPosition) {
+		$scope.UserCurrentPosition = currentPosition;
+  });
+
+  $scope.$watch('UserCurrentPosition', function(){
+		console.log($scope.UserCurrentPosition);
+  });
+  
 
   $scope.onJoinOrderTouch = function(groupOrder) {
 		Order.setCurrentOrder(groupOrder.groupOrderId, groupOrder.timeLeft, groupOrder.currentDiscount);
