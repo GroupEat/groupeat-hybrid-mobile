6fe94b2273341f6180276a2bd3488422cfa5a7a2
Woody Rousseau <woody.rousseau@ensta-paristech.fr>
2015-02-03 17:28:30 +0100

    #67 implemented

--- app/scripts/services/http-provider-interceptor.js
+++ app/scripts/services/http-provider-interceptor.js
@@ -2,9 +2,11 @@
 
 angular.module('groupeat.services.http-provider-interceptor', ['groupeat.services.authentication'])
 
-.factory('HttpProviderInterceptor', function(Authentication) {
+.factory('HttpProviderInterceptor', function($injector) {
   var request = function(config) {
     config.headers.Accept = 'application/vnd.groupeat.v1+json';
+
+    var Authentication = $injector.get('Authentication');
     var credentials = Authentication.getCredentials();
     if (credentials && credentials.token)
     {
