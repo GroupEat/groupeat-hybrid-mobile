8d4b889d2ced76723a4bee002df267c4dbe53902
etiennedemontalivet <etienne.demontalivet@gmail.com>
2015-02-13 16:36:03 +0100

    Connect post order route with simple request body (only products, groupOrderId ; enforcing the others). Begining...

--- app/scripts/services/order.js
+++ app/scripts/services/order.js
@@ -1,54 +1,69 @@
 'use strict';
 
-angular.module('groupeat.services.order', [])
+angular.module('groupeat.services.order', ['ngResource'])
 
-.service('Order',
-	function() {
+.service('Order', function(ENV, $q, $resource) {
+
+	var resource = $resource(ENV.apiEndpoint+'/orders');
 	
-		var currentOrder = {
-				'groupOrderId': null,
-				'timeLeft': null,
-				'currentDiscount': null
-			};
+	var
+	currentOrder = {
+		'groupOrderId': null,
+		'timeLeft': null,
+		'currentDiscount': null
+	},
 
-		var getCurrentOrder = function() {
-			return currentOrder;
+	getCurrentOrder = function() {
+		return currentOrder;
+	},
+
+	resetCurrentOrder = function() {
+		currentOrder = {
+			'groupOrderId': null,
+			'timeLeft': null,
+			'currentDiscount': null
 		};
+	},
 
-		var resetCurrentOrder = function() {
-			currentOrder = {
-				'groupOrderId': null,
-				'timeLeft': null,
-				'currentDiscount': null
-			};
-		};
+	setGroupOrderId = function(id) {
+		currentOrder.groupOrderId = id;
+	},
 
-		var setGroupOrderId = function(id) {
-			currentOrder.groupOrderId = id;
-		};
+	setTimeLeft = function(time) {
+		currentOrder.timeLeft = time;
+	},
 
-		var setTimeLeft = function(time) {
-			currentOrder.timeLeft = time;
-		};
+	setCurrentDiscount = function(discount) {
+		currentOrder.currentDiscount = discount;
+	},
 
-		var setCurrentDiscount = function(discount) {
-			currentOrder.currentDiscount = discount;
-		};
+	setCurrentOrder = function(id, time, discount) {
+		currentOrder.groupOrderId = id;
+		currentOrder.timeLeft = time;
+		currentOrder.currentDiscount = discount;
+	},
 
-		var setCurrentOrder = function(id, time, discount) {
-			currentOrder.groupOrderId = id;
-			currentOrder.timeLeft = time;
-			currentOrder.currentDiscount = discount;
-		};
+	save = function(requestBody) {
+		var defer = $q.defer();
+		resource.save(null, requestBody).$promise
+		.then(function(response) {
+			defer.resolve(response);
+		})
+		.catch(function(errorResponse) {
+			console.log(errorResponse);
+		});
+		return defer.promise;
+	};
 
 
-		return {
-			getCurrentOrder: getCurrentOrder,
-			resetCurrentOrder: resetCurrentOrder,
-			setGroupOrderId: setGroupOrderId,
-			setTimeLeft: setTimeLeft,
-			setCurrentDiscount: setCurrentDiscount,
-			setCurrentOrder: setCurrentOrder
-		};
-	}
+	return {
+		getCurrentOrder: getCurrentOrder,
+		resetCurrentOrder: resetCurrentOrder,
+		setGroupOrderId: setGroupOrderId,
+		setTimeLeft: setTimeLeft,
+		setCurrentDiscount: setCurrentDiscount,
+		setCurrentOrder: setCurrentOrder,
+		save: save
+	};
+}
 );
