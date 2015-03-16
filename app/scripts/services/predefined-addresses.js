f43e0b9a90701c745ac6099669a588bdfc828597
Woody Rousseau <woody.rousseau@ensta-paristech.fr>
2015-03-16 21:23:15 +0100

    Returning promise for predefined addresses

--- app/scripts/services/predefined-addresses.js
+++ app/scripts/services/predefined-addresses.js
@@ -1,7 +1,28 @@
 'use strict';
 
-angular.module('groupeat.services.predefined-addresses', ['ngResource'])
+angular.module('groupeat.services.predefined-addresses', [
+  'config',
+  'ngResource'
+])
 
-.factory('PredefinedAddresses', function($resource, ENV) {
-  return $resource(ENV.apiEndpoint+'/predefinedAddresses');
+.factory('PredefinedAddresses', function($resource, $q, ENV) {
+
+  var resource = $resource(ENV.apiEndpoint+'/predefinedAddresses');
+
+  var
+  get = function() {
+    var defer = $q.defer();
+    resource.get().$promise
+    .then(function(response) {
+      defer.resolve(response.data);
+    })
+    .catch(function() {
+      defer.reject();
+    });
+    return defer.promise;
+  };
+
+  return {
+    get: get
+  };
 });
