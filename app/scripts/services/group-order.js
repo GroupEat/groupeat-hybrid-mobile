0ab3f396d0b599c575dc2c365d827e76d8c508b7
Woody Rousseau <woody.rousseau@ensta-paristech.fr>
2015-03-18 23:31:39 +0100

    Attempting to improve code quality

--- app/scripts/services/group-order.js
+++ app/scripts/services/group-order.js
@@ -1,9 +1,6 @@
 'use strict';
 
-angular.module('groupeat.services.group-order', [
-  'config',
-  'ngResource'
-])
+angular.module('groupeat.services.group-order', ['groupeat.services.backend-utils'])
 
 .factory('GroupOrder', function($resource, $q, ENV) {
 
