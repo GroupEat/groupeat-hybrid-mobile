8c17647d9eed951336efcc0dd6ef6a1d79c7862f
etiennedemontalivet <etienne.demontalivet@gmail.com>
2015-05-18 22:32:46 +0200

    add geolocation service.js

---
+++ app/scripts/services/geolocation.js
@@ -0,0 +1,32 @@
+'use strict';
+
+angular.module('groupeat.services.geolocation', [
+  'ngResource',
+  'ngGeolocation'
+])
+
+
+.factory('Geolocation', function($q, ENV, $geolocation) {
+  var getGeolocation = function() {
+    var defer = $q.defer();
+    if(ENV.name === 'development') {
+      var response = {'coords': {'latitude': 48.7107340, 'longitude': 2.2182329}};
+      defer.resolve(response);
+    }
+    else {
+      $geolocation.getCurrentPosition()
+      .then(function(currentPosition) {
+        defer.resolve(currentPosition);
+      })
+      .catch(function() {
+        defer.reject();
+      });
+    }
+    return defer.promise;
+  };
+
+  return {
+    getGeolocation: getGeolocation
+  };
+
+});
