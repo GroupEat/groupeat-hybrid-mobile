8f3fcb77d99016b0dad98b72d3eb5b63e0a3c3ad
Woody Rousseau <woody.a.rousseau@gmail.com>
2015-06-22 23:48:00 +0200

    Makes MessageBackdrop more flexible

--- app/scripts/services/message-backdrop.js
+++ app/scripts/services/message-backdrop.js
@@ -5,60 +5,80 @@ angular.module('groupeat.services.message-backdrop', [])
 .factory('MessageBackdrop', [
   function() {
 
-    var
+    var /**
+    * @ngdoc function
+    * @name MessageBackdrop#backdrop
+    * @methodOf MessageBackdrop
+    *
+    * @description
+    * Returns a message backdrop object
+    *
+    * @param {String} errorKey
+    * @param {String} iconClasses
+    * @param {String} buttonTitle
+    * @param {String} buttonAction
+    */
+    backdrop = function(errorKey, iconClasses, buttonTitle, buttonAction) {
+      return {
+        show: true,
+        title: errorKey+'Title',
+        details: errorKey+'Details',
+        iconClasses: iconClasses,
+        button: {
+          text: buttonTitle,
+          action: buttonAction
+        }
+      };
+    },
+
+    /**
+    * @ngdoc function
+    * @name MessageBackdrop#backdropFromErrorKey
+    * @methodOf MessageBackdrop
+    *
+    * @description
+    * Returns the appropriate message backdrop or a generic failure default
+    *
+    * @param {String} errorKey
+    */
+    backdropFromErrorKey = function(errorKey) {
+      var fn = this[errorKey];
+      if (typeof fn === 'function') {
+        return fn();
+      }
+      return genericFailure();
+    },
+
+    /**
+    * @ngdoc function
+    * @name MessageBackdrop#noBackdrop
+    * @methodOf MessageBackdrop
+    *
+    * @return Returns the appropriate message backdrop or a generic failure default
+    *
+    */
     noBackdrop = function() {
       return {
         show: false
       };
     },
 
-    noNetwork = function(buttonAction) {
-      buttonAction = buttonAction || 'onReload()';
-      return {
-        show: true,
-        title: 'noNetworkTitle',
-        details: 'noNetworkDetails',
-        iconClasses: 'ion-wifi',
-        button: {
-          text: 'reload',
-          action: buttonAction
-        },
-      };
+    noNetwork = function() {
+      return backdrop('noNetwork', 'ion-wifi', 'reload', 'onReload()');
     },
 
-    noGeolocation = function(buttonAction) {
-      buttonAction = buttonAction || 'onReload()';
-      return {
-        show: true,
-        title: 'noGeolocationTitle',
-        details: 'noGeolocationDetails',
-        iconClasses: 'ion-location',
-        button: {
-          text: 'reload',
-          action: buttonAction
-        },
-      };
+    noGeolocation = function() {
+      return backdrop('noGeolocation', 'ion-wifi', 'reload', 'onReload()');
     },
 
-    genericFailure = function(buttonAction) {
-      buttonAction = buttonAction || 'onReload()';
-      return {
-        show: true,
-        title: 'whoops',
-        details: 'genericFailureDetails',
-        iconClasses: 'ion-alert-circled',
-        button: {
-          text: 'reload',
-          action: buttonAction
-        }
-      };
+    genericFailure = function() {
+      return backdrop('genericFailure', 'ion-alert-circle', 'reload', 'onReload()');
     };
 
     return {
+      backdrop: backdrop,
+      backdropFromErrorKey: backdropFromErrorKey,
       noBackdrop: noBackdrop,
-      noNetwork: noNetwork,
-      noGeolocation: noGeolocation,
-      genericFailure: genericFailure,
     };
 
   }
