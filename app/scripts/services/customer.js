b4361eba05308a14c1a0d2671c3b042ebde7cb0b
Woody Rousseau <woody.a.rousseau@gmail.com>
2015-06-29 21:44:17 +0200

    Redirects to authentication when customer get returns a 404

--- app/scripts/services/customer.js
+++ app/scripts/services/customer.js
@@ -36,7 +36,10 @@ angular.module('groupeat.services.customer', [
     .then(function(response) {
       defer.resolve(response.data);
     })
-    .catch(function() {
+    .catch(function(errorResponse) {
+      if (errorResponse.status === 404) {
+        $state.go('authentication');
+      }
       defer.reject();
     });
     return defer.promise;
