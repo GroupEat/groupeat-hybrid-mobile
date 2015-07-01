f090845d212a434b38ee1440b4104fa556fe659a
Jonathan Widawski <jonathan.widawski@gmail.com>
2015-07-01 18:09:46 +0200

    Navbar fixed

--- app/scripts/directives/header-shrink.js
+++ app/scripts/directives/header-shrink.js
@@ -7,27 +7,28 @@ angular.module('groupeat.directives.header-shrink', []).directive('headerShrink'
       var header = $document[0].body.querySelector(attr.headerShrink);
       var content = $document[0].body.querySelector(attr.headerShrink + '-content');
       $timeout(function () {
-        navbar = $document[0].body.querySelector('.bar-header');
-        element.bind('scroll', function(e) {
+        navbar = $document[0].body.querySelector('.nav-bar-block .bar-header');
+        console.log(navbar);
+      });
+      element.bind('scroll', function(e) {
         var scrollTop = e.originalEvent.detail.scrollTop;
-          if (scrollTop >= 0) {
-            scrollFactor = scrollTop/2;
-            opacityFactor = scrollTop/150;
-            amt = Math.min(44, scrollTop / 6);
-            backgroundAmt = amt / 44;
-            header.style[ionic.CSS.TRANSFORM] = 'translate3d(0, +' + scrollFactor + 'px, 0)';
-            navbar.style.backgroundColor = 'rgba(48,54,76,' + backgroundAmt + ') !important';
-            content.style.opacity = 1 - opacityFactor;
-          } else {
-            resizeFactor = -scrollTop/100 + 0.99;
-            blurFactor = -scrollTop/10;
-            header.style[ionic.CSS.TRANSFORM] = 'scale('+resizeFactor+','+resizeFactor+')';
-            navbar.style.backgroundColor = 'rgba(48,54,76,0) !important';
-            content.style.opacity = 1 - blurFactor/6;
-            header.style.webkitFilter = 'blur('+blurFactor+'px)';
-          }
-        });
-      }, 1000);
+        if (scrollTop >= 0) {
+          scrollFactor = scrollTop/2;
+          opacityFactor = scrollTop/150;
+          amt = Math.min(44, scrollTop / 6);
+          backgroundAmt = amt / 44;
+          header.style[ionic.CSS.TRANSFORM] = 'translate3d(0, +' + scrollFactor + 'px, 0)';
+          navbar.style.backgroundColor = 'rgba(48,54,76,' + backgroundAmt + ')';
+          content.style.opacity = 1 - opacityFactor;
+        } else {
+          resizeFactor = -scrollTop/100 + 0.99;
+          blurFactor = -scrollTop/10;
+          header.style[ionic.CSS.TRANSFORM] = 'scale('+resizeFactor+','+resizeFactor+')';
+          navbar.style.backgroundColor = 'rgba(48,54,76,0)';
+          content.style.opacity = 1 - blurFactor/6;
+          header.style.webkitFilter = 'blur('+blurFactor+'px)';
+        }
+      });
     }
   };
 });
