e8b40a10624269a5ccc326ee943a93b5af9e90d5
Woody Rousseau <woody.a.rousseau@gmail.com>
2015-07-05 20:35:34 +0200

    Moves onRestaurantTouch in controller

--- app/scripts/directives/restaurant-card.js
+++ app/scripts/directives/restaurant-card.js
@@ -1,6 +1,9 @@
 'use strict';
 
-angular.module('groupeat.directives.restaurant-card', []).directive('restaurantCard', function (Customer, GroupOrder, Order, Restaurant, $state) {
+angular.module('groupeat.directives.restaurant-card', [])
+
+.directive('geRestaurantCard', function () {
+
   return {
     restrict: 'EA',
     templateUrl: 'templates/cards/restaurant-card.html',
@@ -8,20 +11,6 @@ angular.module('groupeat.directives.restaurant-card', []).directive('restaurantC
     link: function (scope) {
       scope.setArrayFromInt = function (num) {
         return new Array(num);
-      };
-      scope.onRestaurantTouch = function(restaurant) {
-        Customer.checkActivatedAccount()
-        .then(function() {
-          return Customer.checkMissingInformation();
-        }).then(function () {
-          
-        }).then(function (groupOrders) {
-          return Restaurant.checkGroupOrders(restaurant.id, groupOrders);
-        })
-        .then(function() {
-          Order.setCurrentOrder(null, null, 0, restaurant.deliveryCapacity, restaurant.discountPolicy);
-          $state.go('restaurant-menu', {restaurantId: restaurant.id});
-        });
       };
     }
   };
