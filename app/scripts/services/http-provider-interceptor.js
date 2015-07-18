e1b7646a41c23e26caa2cf2019b850a5454adae6
Woody Rousseau <woody.a.rousseau@gmail.com>
2015-07-18 20:40:58 +0200

    Disables interceptor on ionic.io requests

--- app/scripts/services/http-provider-interceptor.js
+++ app/scripts/services/http-provider-interceptor.js
@@ -1,11 +1,14 @@
 'use strict';
 angular.module('groupeat.services.http-provider-interceptor', ['groupeat.services.credentials']).factory('HttpProviderInterceptor', function ($injector, Credentials) {
   var request = function (config) {
-    config.headers.Accept = 'application/vnd.groupeat.v1+json';
-    var credentials = Credentials.get();
-    if (credentials && credentials.token) {
-      config.headers.Authorization = 'bearer ' + credentials.token;
+    if (config.url.indexOf('ionic.io') === -1) {
+      config.headers.Accept = 'application/vnd.groupeat.v1+json';
+      var credentials = Credentials.get();
+      if (credentials && credentials.token) {
+        config.headers.Authorization = 'bearer ' + credentials.token;
+      }
     }
+
     return config;
   };
   return { request: request };
