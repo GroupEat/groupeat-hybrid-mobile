d308fdf9af8564ba5660c6bf731aae1ba1926635
Woody Rousseau <woody.a.rousseau@gmail.com>
2015-07-18 20:40:45 +0200

    Uses ionic analytics

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,14 +1,10 @@
 'use strict';
-// Ionic Starter App, v0.9.20
-// angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
-// the 2nd parameter is an array of 'requires'
-// 'starter.services' is found in services.js
-// 'starter.controllers' is found in controllers.js
+
 angular.module('groupeat', [
   'config',
   'constants',
   'ionic',
+  'ionic.service.analytics',
   'jcs-autoValidate',
   'ngCordova',
   'ngCookies',
@@ -26,7 +22,10 @@ angular.module('groupeat', [
   'groupeat.services.error-message-resolver',
   'slick',
   'ui.mask'
-]).run([
+  'slick'
+])
+
+.run([
   'validator',
   'ElementModifier',
   'ErrorMessageResolver',
@@ -35,9 +34,10 @@ angular.module('groupeat', [
     validator.setDefaultElementModifier(ElementModifier.key);
     validator.setErrorMessageResolver(ErrorMessageResolver.resolve);
   }
-]).run(function ($ionicPlatform, $translate, $rootScope, $state, Analytics, Credentials, Permission) {
+])
+
+.run(function ($ionicPlatform, $ionicAnalytics, $translate, $rootScope, $state, Analytics, Credentials, Permission) {
   Permission.defineRole('customer', function () {
-    // If the returned value is *truthy* then the user has the role, otherwise they don't
     return Credentials.get();
   });
   if (typeof navigator.globalization !== 'undefined') {
@@ -50,19 +50,15 @@ angular.module('groupeat', [
     }, null);
   }
   $ionicPlatform.ready(function () {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if (window.cordova && window.cordova.plugins.Keyboard) {
+
+    $ionicAnalytics.register();
+
+    if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
     }
     if (window.StatusBar) {
-      // org.apache.cordova.statusbar required
       StatusBar.styleDefault();
     }
     Analytics.startTrackerWithId('UA-62863405-1');
   });
-});  // .run(function($cordovaSplashscreen) {
-     //   setTimeout(function() {
-     //     $cordovaSplashscreen.hide();
-     //   }, 5000);
-     // });
+});
