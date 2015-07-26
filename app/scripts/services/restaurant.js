bca4dee76611730378440c66882b9068ca204cb4
Woody Rousseau <woody.a.rousseau@gmail.com>
2015-07-27 00:37:41 +0200

    Removes angular material from Restaurant service

--- app/scripts/services/restaurant.js
+++ app/scripts/services/restaurant.js
@@ -1,14 +1,20 @@
 'use strict';
+
 angular.module('groupeat.services.restaurant', [
   'groupeat.services.backend-utils',
   'groupeat.services.lodash',
-  'ngMaterial'
-]).factory('Restaurant', function (_, $filter, $mdDialog, $resource, $q, ENV) {
+  'groupeat.services.popup'
+])
+
+.factory('Restaurant', function (_, $filter, Popup, $resource, $q, ENV) {
+
   var $translate = $filter('translate');
-  var resourceId = $resource(ENV.apiEndpoint + '/restaurants/:id');
-  var resource = $resource(ENV.apiEndpoint + '/restaurants?opened=1&around=1&latitude=:latitude&longitude=:longitude');
+
+  var resource = $resource(ENV.apiEndpoint + '/restaurants/:id');
+  var listResource = $resource(ENV.apiEndpoint + '/restaurants?opened=1&around=1&latitude=:latitude&longitude=:longitude');
+
   var
-    /**
+  /**
   * @ngdoc function
   * @name Restaurant#getFromCoordinates
   * @methodOf Restaurant
@@ -19,42 +25,42 @@ angular.module('groupeat.services.restaurant', [
   * https://groupeat.fr/docs
   *
   */
-    getFromCoordinates = function (latitude, longitude) {
-      var defer = $q.defer();
-      resource.get({
-        latitude: latitude,
-        longitude: longitude
-      }).$promise.then(function (response) {
-        defer.resolve(response.data);
-      }).catch(function () {
-        defer.reject();
-      });
-      return defer.promise;
-    },
+  getFromCoordinates = function (latitude, longitude) {
+    var defer = $q.defer();
+    listResource.get({latitude: latitude, longitude: longitude}).$promise
+    .then(function (response) {
+      defer.resolve(response.data);
+    })
+    .catch(function () {
+      defer.reject();
+    });
+    return defer.promise;
+  },
 
-    /**
+  /**
   * @ngdoc function
   * @name Restaurant#get
   * @methodOf Restaurant
   *
   * @description
-  * Returns a promise resolved with the list of currently opened restaurants if the server responds properly
+  * Returns a promise resolved with the restaurant
   * Else the promise is rejected
   * https://groupeat.fr/docs
   *
   */
-    get = function (restaurantId) {
-      var defer = $q.defer();
-      resourceId.get({
-        id: restaurantId
-      }).$promise.then(function (response) {
-        defer.resolve(response.data);
-      }).catch(function () {
-        defer.reject();
-      });
-      return defer.promise;
-    },
-    /**
+  get = function (restaurantId) {
+    var defer = $q.defer();
+    resource.get({id: restaurantId}).$promise
+    .then(function (response) {
+      defer.resolve(response.data);
+    })
+    .catch(function () {
+      defer.reject();
+    });
+    return defer.promise;
+  },
+
+  /**
   * @ngdoc function
   * @name Customer#checkGroupOrders
   * @methodOf Customer
@@ -63,15 +69,19 @@ angular.module('groupeat.services.restaurant', [
   *
   * @description Returns a promise informing wether or not the customer has already activated his/her account
   */
-    checkGroupOrders = function (restaurantId, groupOrders) {
-      var deferred = $q.defer();
-      if (_.some(groupOrders, 'restaurant.data.id', restaurantId)) {
-        var confirm = $mdDialog.confirm({ parent: angular.element(document.body) }).title($translate('restaurantHasGroupOrderTitle')).content($translate('restaurantHasGroupOrderMessage')).ok($translate('join')).cancel($translate('cancel'));
-        return $mdDialog.show(confirm);
-      }
+  checkGroupOrders = function (restaurantId, groupOrders) {
+    var deferred = $q.defer();
+    if (_.some(groupOrders, 'restaurant.data.id', restaurantId)) {
+      Popup.confirm('restaurantHasGroupOrder', 'restaurantHasGroupOrderMessage', 'join')
+      .then(function(res) {
+        res ? deferred.resolve() : deferred.reject();
+      });
+    } else {
       deferred.resolve();
-      return deferred.promise;
-    };
+    }
+    return deferred.promise;
+  };
+
   return {
     checkGroupOrders: checkGroupOrders,
     get: get,
