0bd7c9f9c57dad7558a3e46d0ef1ae58953b9320
Woody Rousseau <woody.a.rousseau@gmail.com>
2015-07-26 22:18:44 +0200

    Adds MessageBackdrop directive

--- app/scripts/directives/message-backdrop.js
+++ app/scripts/directives/message-backdrop.js
@@ -1,14 +1,36 @@
 'use strict';
-angular.module('groupeat.directives.message-backdrop', ['pascalprecht.translate']).directive('geMessageBackdrop', function ($timeout) {
+
+angular.module('groupeat.directives.message-backdrop', [
+  'ui.router',
+  'pascalprecht.translate'
+])
+
+.directive('geMessageBackdrop', function ($rootScope, $state) {
   return {
-    restrict: 'E',
-    scope: true,
-    templateUrl: 'templates/message-backdrop.html',
-    link: function (scope) {
-      scope.call = function (methodName) {
-        $timeout(function () {
-          scope.$apply(methodName);
-        });
+    restrict: 'EA',
+    templateUrl: 'templates/layouts/message-backdrop.html',
+    scope: {
+      status: '=',
+      icon: '=',
+      title: '=',
+      details: '=',
+      buttonText: '=',
+      buttonSref: '='
+    },
+    link: function(scope) {
+      scope.isLoading = function() {
+        return scope.status === 'loading';
+      };
+      scope.isDisplayed = function() {
+        return scope.status === 'displayed';
+      };
+      scope.buttonAction = function() {
+        if(scope.buttonSref) {
+          $state.go(scope.buttonSref);
+        } else {
+          $state.reload();
+          $rootScope.$broadcast('$ionicView.afterEnter');
+        }
       };
     }
   };
