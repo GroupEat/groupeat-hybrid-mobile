0478d519849eb96664984f13cb837e5c64bc4cc2
Jonathan Widawski <jonathan.widawski@gmail.com>
2015-07-28 22:27:48 +0200

    Removing unnecesary code

--- app/scripts/directives/receipt-card.js
+++ app/scripts/directives/receipt-card.js
@@ -14,7 +14,6 @@ angular.module('groupeat.directives.receipt-card', []).directive('receiptCard', 
       scope.setArrayFromInt = function (num) {
         return new Array(num);
       };
-      console.log(scope.displayComment);
       scope.date = new Date();
       scope.$watch('discount', function() {
         scope.realPrice = scope.orders.getTotalPrice() * ( (100 - scope.discount ) / 100 );
