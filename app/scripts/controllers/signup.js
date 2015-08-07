5e7eef59496c61ee3ef583492a35a645b74c53f6
Woody Rousseau <woody.a.rousseau@gmail.com>
2015-08-07 22:20:55 +0200

    Provides signup functionnality

--- app/scripts/controllers/signup.js
+++ app/scripts/controllers/signup.js
@@ -1,15 +1,43 @@
 'use strict';
 
-angular.module('groupeat.controllers.signup', [])
+angular.module('groupeat.controllers.signup', [
+  'groupeat.services.address',
+  'groupeat.services.credentials',
+  'groupeat.services.customer',
+  'groupeat.services.lodash',
+  'groupeat.services.network',
+  'groupeat.services.popup',
+  'ionic'
+])
 
-.controller('SignupCtrl', function ($scope, $ionicSlideBoxDelegate) {
-  
+.controller('SignupCtrl', function (_, $ionicSlideBoxDelegate, $scope, $state, Address, Credentials, Customer, Network, Popup) {
+
   $scope.slideIndex = 0;
-  $scope.address = 0;
+  $scope.user = {};
 
   $scope.slideTo = function(index) {
     $ionicSlideBoxDelegate.slide(index);
     $scope.slideIndex = index;
+  };
+
+  $scope.confirmSignup = function() {
+    Network.hasConnectivity()
+    .then(function() {
+      var customerId = Credentials.get().id;
+      var customerParams = _.pick($scope.user, ['firstName', 'lastName']);
+      customerParams.phoneNumber = '0'+$scope.user.phoneNumber;
+      return Customer.update(customerId, customerParams);
+    })
+    .then(function(customer) {
+      var addressParams = _.merge(Address.getAddressFromResidencyInformation($scope.user.residency), {details: $scope.user.addressSupplement});
+      return Address.update(customer.id, addressParams);
+    })
+    .then(function() {
+      $state.go('app.group-orders');
+    })
+    .catch(function(errorMessage) {
+      return Popup.error(errorMessage);
+    });
   };
 
 });
