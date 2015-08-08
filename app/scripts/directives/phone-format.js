1930dd347842a410771883ccfe32485060a07f6c
Thibault Derousseaux <tib.dex@gmail.com>
2015-08-08 17:49:29 +0200

    Enforcing a stricter phone format and removing +33 from signUp view

--- app/scripts/directives/phone-format.js
+++ app/scripts/directives/phone-format.js
@@ -8,7 +8,7 @@ angular.module('groupeat.directives.phone-format', []).directive('gePhoneFormat'
           return true;
         }
         // Basic email matching
-        var regex = /^0[0-9]([ .-]?[0-9]{2}){4}$/i;
+        var regex = /^[0-9]{10}$/i;
         var matches = regex.exec(viewValue);
         return matches !== null;
       };
