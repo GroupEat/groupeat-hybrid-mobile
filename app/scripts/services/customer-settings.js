a457693850a43c254028594abc92f21ef89cce27
etiennedemontalivet <etiennedemontalivet@gmail.com>
2015-08-23 13:15:29 +0200

    upgrade customer settings service

--- app/scripts/services/customer-settings.js
+++ app/scripts/services/customer-settings.js
@@ -2,44 +2,57 @@
 angular.module('groupeat.services.customer-settings', [
   'constants',
   'groupeat.services.backend-utils',
+  'groupeat.services.lodash',
   'ngResource'
-]).factory('CustomerSettings', function ($resource, $q, BackendUtils, ENV) {
+]).factory('CustomerSettings', function ($resource, $q, BackendUtils, ENV, _) {
   var resource = $resource(ENV.apiEndpoint + '/customers/:id/settings', null, { 'update': { method: 'PUT' } });
-  var getNoNotificationAfterHours = function () {
-      return [
-        {
-          value: '21:00:00',
-          label: '21h00'
-        },
-        {
-          value: '22:00:00',
-          label: '22h00'
-        },
-        {
-          value: '23:00:00',
-          label: '23h00'
-        },
-        {
-          value: '00:00:00',
-          label: '00h00'
-        },
-        {
-          value: '01:00:00',
-          label: '01h00'
-        }
-      ];
-    }, getDaysWithoutNotifying = function () {
-      return [
-        0,
-        1,
-        2,
-        3,
-        4,
-        5,
-        6,
-        7
-      ];
+  var noNotificationAfterHours = [
+    {
+      value: '21:00:00',
+      label: '21h00'
     },
+    {
+      value: '22:00:00',
+      label: '22h00'
+    },
+    {
+      value: '23:00:00',
+      label: '23h00'
+    },
+    {
+      value: '00:00:00',
+      label: '00h00'
+    },
+    {
+      value: '01:00:00',
+      label: '01h00'
+    }
+  ];
+
+  var getNoNotificationAfterHours = function () {
+    return _.pluck(noNotificationAfterHours, 'value');
+
+  },
+
+  getDaysWithoutNotifying = function () {
+    return [
+    0,
+    1,
+    2,
+    3,
+    4,
+    5,
+    6,
+    7
+    ];
+  }, 
+
+  getLabelHourFromValue = function(value) {
+    var matchHour = _.find(noNotificationAfterHours, function(hour) {
+      return hour.value === value
+    });
+    return matchHour.label;
+  },
     /**
   * @ngdoc function
   * @name CustomerSettings#get
@@ -50,15 +63,15 @@ angular.module('groupeat.services.customer-settings', [
   * https://groupeat.fr/docs
   *
   */
-    get = function (customerId) {
-      var defer = $q.defer();
-      resource.get({ id: customerId }).$promise.then(function (response) {
-        defer.resolve(response.data);
-      }).catch(function () {
-        defer.reject();
-      });
-      return defer.promise;
-    },
+  get = function (customerId) {
+    var defer = $q.defer();
+    resource.get({ id: customerId }).$promise.then(function (response) {
+      defer.resolve(response.data);
+    }).catch(function () {
+      defer.reject();
+    });
+    return defer.promise;
+  },
     /**
   * @ngdoc function
   * @name CustomerSettings#update
@@ -69,19 +82,20 @@ angular.module('groupeat.services.customer-settings', [
   * https://groupeat.fr/docs
   *
   */
-    update = function (customerId, customerSettings) {
-      var defer = $q.defer();
-      resource.update({ id: customerId }, customerSettings).$promise.then(function (response) {
-        defer.resolve(response.data);
-      }).catch(function (errorResponse) {
-        defer.reject(BackendUtils.errorMsgFromBackend(errorResponse));
-      });
-      return defer.promise;
-    };
+  update = function (customerId, customerSettings) {
+    var defer = $q.defer();
+    resource.update({ id: customerId }, customerSettings).$promise.then(function (response) {
+      defer.resolve(response.data);
+    }).catch(function (errorResponse) {
+      defer.reject(BackendUtils.errorMsgFromBackend(errorResponse));
+    });
+    return defer.promise;
+  };
   return {
     getNoNotificationAfterHours: getNoNotificationAfterHours,
     getDaysWithoutNotifying: getDaysWithoutNotifying,
     get: get,
+    getLabelHourFromValue: getLabelHourFromValue,
     update: update
   };
 });
