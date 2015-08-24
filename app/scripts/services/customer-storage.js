d753b8f241f3e6eee9232b9f46adc75d13a5e71d
etiennedemontalivet <etiennedemontalivet@gmail.com>
2015-08-24 21:16:46 +0200

    finish to integrate woodys feedbacks

--- app/scripts/services/customer-storage.js
+++ app/scripts/services/customer-storage.js
@@ -1,89 +1,57 @@
 'use strict';
 
 angular.module('groupeat.services.customer-storage', [
-  'LocalStorageModule',
-  'ui.router'
+  'LocalStorageModule'
 ])
 
 .factory('CustomerStorage', function (localStorageService) {
 
   var
   setAddress = function(address) {
-    if(address.residency) {
-      localStorageService.set('residency', address.residency);
-    }
-    if(address.details) {
-      localStorageService.set('details', address.details);
+    if(address) {
+      localStorageService.set('address', address);
     }
   },
 
   setIdentity = function(identity) {
-    if(identity.firstName) {
-      localStorageService.set('firstName', identity.firstName);  
-    }
-    if(identity.lastName) {
-      localStorageService.set('lastName', identity.lastName);
-    }
-    if(identity.phoneNumber) {
-      localStorageService.set('phoneNumber', identity.phoneNumber);
-    }
-    if(identity.email) {
-      localStorageService.set('email', identity.email);
+    if(identity) {
+      localStorageService.set('identity', identity);
     }
   },
 
   setSettings = function(settings) {
-    if(settings.notificationsEnabled !== undefined) {
-      localStorageService.set('notificationsEnabled', settings.notificationsEnabled);
-    }
-    if(settings.daysWithoutNotifying !== undefined) {
-      localStorageService.set('daysWithoutNotifying', settings.daysWithoutNotifying);
-    }
-    if(settings.daysWithoutNotifying) {
-      localStorageService.set('noNotificationAfter', settings.noNotificationAfter);
+    if(settings) {
+      localStorageService.set('settings', settings);
     }
   },
 
   setDefaultSettings = function() {
-    localStorageService.set('notificationsEnabled', true);
-    localStorageService.set('daysWithoutNotifying', '2');
-    localStorageService.set('noNotificationAfter', '23:00:00');
+    var settings = {
+      'notificationsEnabled' : 'true',
+      'daysWithoutNotifying': '2',
+      'noNotificationAfter': '23:00:00' 
+    };
+    localStorageService.set('settings', settings);
   },
 
   reset = function () {
-    localStorageService.remove('firstName');
-    localStorageService.remove('lastName');
-    localStorageService.remove('phoneNumber');
-    localStorageService.remove('email');
-    localStorageService.remove('residency');
-    localStorageService.remove('details');
-    localStorageService.remove('notificationsEnabled');
-    localStorageService.remove('daysWithoutNotifying');
-    localStorageService.remove('noNotificationAfter');
+    localStorageService.remove('settings');
+    localStorageService.remove('identity');
+    localStorageService.remove('settings');
   },
 
   getAddress = function() {
-    var address = {};
-    address.residency = localStorageService.get('residency');
-    address.details = localStorageService.get('details');
-    return address;
+    return localStorageService.get('address') || {};
   },
 
   getIdentity = function() {
-    var identity = {};
-    identity.firstName = localStorageService.get('firstName');
-    identity.lastName = localStorageService.get('lastName');
-    identity.phoneNumber = localStorageService.get('phoneNumber');
-    identity.email = localStorageService.get('email');
-    return identity;
+    return localStorageService.get('identity') || {};
   },
 
   getSettings = function() {
-    var settings = {};
-    settings.notificationsEnabled = ( localStorageService.get('notificationsEnabled') === 'true' );
-    settings.daysWithoutNotifying = localStorageService.get('daysWithoutNotifying');
-    settings.noNotificationAfter = localStorageService.get('noNotificationAfter');
-    return settings;
+    var settings = localStorageService.get('settings');
+    settings.notificationsEnabled = (settings.notificationsEnabled === 'true');
+    return settings || {};
   };
 
   return {
