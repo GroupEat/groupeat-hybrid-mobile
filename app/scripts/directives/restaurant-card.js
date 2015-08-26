9b4c176d732cf5c555bc128f73d051f71411d958
Guillaume Diallo-Mulliez <guillaume.diallo@ensta-paristech.fr>
2015-08-27 01:41:14 +0200

    Connect rating to backend and display stars

--- app/scripts/directives/restaurant-card.js
+++ app/scripts/directives/restaurant-card.js
@@ -11,9 +11,23 @@ angular.module('groupeat.directives.restaurant-card', [
     templateUrl: 'templates/cards/restaurant-card.html',
     scope: { data: '=' },
     link: function (scope) {
+
+      var rating = 2 * scope.data.rating;
+
       scope.setArrayFromInt = function (num) {
         return new Array(num);
       };
+
+      scope.starClass = function(index) {
+        console.log(rating/2);
+        if (index < Math.floor(rating / 2)) {
+          return 'icon ion-ios-star';
+        } else if (index === Math.floor(rating / 2) && rating % 2 === 1){
+          return 'icon ion-ios-star-half';
+        }
+        return 'icon ion-ios-star-outline';
+      };
+
     }
   };
 });
