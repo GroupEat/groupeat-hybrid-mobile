29a33549831ab4d878812c80d8ac7502572839cd
etiennedemontalivet <etiennedemontalivet@gmail.com>
2015-08-29 18:21:05 +0200

    create new directive card for orders

--- app/scripts/directives/directives.js
+++ app/scripts/directives/directives.js
@@ -5,6 +5,7 @@ angular.module('groupeat.directives', [
   'groupeat.directives.hide-tabs',
   'groupeat.directives.touch',
   'groupeat.directives.order-card',
+  'groupeat.directives.orders-card',
   'groupeat.directives.nav-bar-class',
   'groupeat.directives.message-backdrop',
   'groupeat.directives.header-shrink',
