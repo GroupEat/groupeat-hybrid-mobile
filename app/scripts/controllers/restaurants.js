d8440ecf0f24948d781f59234afd52fb457fe0b5
etiennedemontalivet <etiennedemontalivet@gmail.com>
2015-09-07 21:01:28 +0200

    disable join restaurant and reload in restaurant ctrl when requesting

--- app/scripts/controllers/restaurants.js
+++ app/scripts/controllers/restaurants.js
@@ -18,49 +18,60 @@ angular.module('groupeat.controllers.restaurants', [
   Analytics.trackView('Restaurants');
 
   $scope.restaurants = [];
+  $scope.isRequesting = false;
 
   $scope.onReload = function() {
-    var promise = Network.hasConnectivity()
-    .then(function() {
-      return Geolocation.getGeolocation();
-    })
-    .then(function(currentPosition) {
-      $scope.userCurrentPosition = currentPosition;
-      return Restaurant.getFromCoordinates(currentPosition.coords.latitude, currentPosition.coords.longitude);
-    })
-    .then(function(restaurants) {
-      if (_.isEmpty(restaurants)) {
-        return $q.reject('noRestaurants');
-      } else {
-        $scope.restaurants = restaurants;
-      }
-    });
-    ControllerPromiseHandler.handle(promise, $scope.initialState)
-    .finally(function() {
-      $scope.$broadcast('scroll.refreshComplete');
-    });
+    if(!$scope.isRequesting) {
+      $scope.isRequesting = true;
+      var promise = Network.hasConnectivity()
+      .then(function() {
+        return Geolocation.getGeolocation();
+      })
+      .then(function(currentPosition) {
+        $scope.userCurrentPosition = currentPosition;
+        return Restaurant.getFromCoordinates(currentPosition.coords.latitude, currentPosition.coords.longitude);
+      })
+      .then(function(restaurants) {
+        if (_.isEmpty(restaurants)) {
+          return $q.reject('noRestaurants');
+        } else {
+          $scope.restaurants = restaurants;
+        }
+      });
+      ControllerPromiseHandler.handle(promise, $scope.initialState)
+      .finally(function() {
+        $scope.$broadcast('scroll.refreshComplete');
+        $scope.isRequesting = false;
+      });
+    }
   };
 
   $scope.onRestaurantTouch = function(restaurant) {
-    Customer.checkActivatedAccount()
-    .then(function() {
-      return CustomerInformationChecker.check();
-    })
-    .then(function () {
-      return GroupOrder.get($scope.userCurrentPosition.coords.latitude, $scope.userCurrentPosition.coords.longitude);
-    })
-    .then(function (groupOrders) {
-      return Restaurant.checkGroupOrders(restaurant.id, groupOrders);
-    })
-    .then(function(existingGroupOrder) {
-      if (existingGroupOrder) {
-        Order.setCurrentOrder(existingGroupOrder.id, existingGroupOrder.endingAt, existingGroupOrder.discountRate, existingGroupOrder.remainingCapacity, existingGroupOrder.restaurant.data.discountPolicy, existingGroupOrder.totalRawPrice);
-      }
-      else {
-        Order.setCurrentOrder(null, null, 0, restaurant.deliveryCapacity, restaurant.discountPolicy);
-      }
-      $state.go('app.restaurant-menu', {restaurantId: restaurant.id});
-    });
+    if(!$scope.isRequesting) {
+      $scope.isRequesting = true;
+      Customer.checkActivatedAccount()
+      .then(function() {
+        return CustomerInformationChecker.check();
+      })
+      .then(function () {
+        return GroupOrder.get($scope.userCurrentPosition.coords.latitude, $scope.userCurrentPosition.coords.longitude);
+      })
+      .then(function (groupOrders) {
+        return Restaurant.checkGroupOrders(restaurant.id, groupOrders);
+      })
+      .then(function(existingGroupOrder) {
+        if (existingGroupOrder) {
+          Order.setCurrentOrder(existingGroupOrder.id, existingGroupOrder.endingAt, existingGroupOrder.discountRate, existingGroupOrder.remainingCapacity, existingGroupOrder.restaurant.data.discountPolicy, existingGroupOrder.totalRawPrice);
+        }
+        else {
+          Order.setCurrentOrder(null, null, 0, restaurant.deliveryCapacity, restaurant.discountPolicy);
+        }
+        $state.go('app.restaurant-menu', {restaurantId: restaurant.id});
+      })
+      .finally(function() {
+        $scope.isRequesting = false;
+      });
+    }
   };
 
   $scope.$on('$ionicView.afterEnter', function() {
