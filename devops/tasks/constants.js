f281fc62f0a8526503945b98473ea81ba4799041
Woody Rousseau <woody.a.rousseau@gmail.com>
2015-11-18 01:21:20 +0100

    Improves build by cleaning plugins and making ionic task work better

--- devops/tasks/constants.js
+++ devops/tasks/constants.js
@@ -1,26 +1,18 @@
 var gulp = require('gulp');
 var ngConstant = require('gulp-ng-constant');
 var _ = require('lodash');
+var argv = require('yargs')
+.default('env', 'staging')
+.describe('env', 'the environment to use')
+.choices('env', ['development', 'staging', 'production'])
+.argv;
 
 var minimist = require('minimist');
 
 gulp.task('constants', function () {
-  var knownOptions = {
-    string: 'env',
-    default: { env: process.env.NODE_ENV || 'production' }
-  };
-
-  var options = minimist(process.argv.slice(2), knownOptions);
-
-  if (options.env && _.includes(['development', 'staging', 'production'])) {
-    env = options.env;
-  } else {
-    env = 'staging';
-    console.error('Using staging environment (none or incorrect environment provided)');
-  }
 
   var myConfig = require('./config.json');
-  var envConfig = myConfig[env];
+  var envConfig = myConfig[argv.env];
   return ngConstant({
       constants: envConfig,
       stream: true,
