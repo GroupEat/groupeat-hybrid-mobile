285dea32a2b522991c32e002ffcc30f3ff1f5705
Woody Rousseau <woody.a.rousseau@gmail.com>
2015-11-30 00:29:59 +0100

    Removes need to inject manually files in karma

--- devops/tasks/test.js
+++ devops/tasks/test.js
@@ -1,10 +1,26 @@
+var bowerFiles = require('main-bower-files');
 var gulp = require('gulp');
+var inject = require('gulp-inject');
 var Server = require('karma').Server;
+
+gulp.task('test:configure', function (done) {
+  gulp.src('test/karma.conf.js')
+  .pipe(inject(gulp.src(bowerFiles('**/*.js'), {read: false}), {
+    starttag: '//<!-- bower -->',
+    endtag: '//<!-- endbower -->',
+    relative: true,
+    transform: function (filepath, file, i, length) {
+      return '"' + filepath + '",';
+    }
+  }))
+  .pipe(gulp.dest('test'))
+  .on('end', done);
+});
 
 /**
  * Run test once and exit
  */
-gulp.task('test', function (done) {
+gulp.task('test', ['test:configure'], function (done) {
   new Server({
     configFile: __dirname + '/../../test/karma.conf.js',
     singleRun: true
@@ -14,7 +30,7 @@ gulp.task('test', function (done) {
 /**
  * Watch for file changes and re-run tests on each change
  */
-gulp.task('tdd', function (done) {
+gulp.task('tdd', ['test:configure'], function (done) {
   new Server({
     configFile: __dirname + '/../../test/karma.conf.js',
   }, done).start();
